375c0b9b7dae0ec3bbc3df8537a8788abed6018e
Cole Wang <cole@andestech.com>
2012-04-02 14:20:17 +0800

    (sim) Update simulator testcases.

--- sim/testsuite/sim/nds32/signext.s
+++ sim/testsuite/sim/nds32/signext.s
@@ -29,28 +29,38 @@ main:
 	ori	$r3, $r3, 32	!psw.be
 	mtsr	$r3, $psw
 
-	movi	$r0, 0
+	movi	$r4, 0
 
 	! HALF
 	la	$r3, HALF
 	move	$r2, 0x8384
 	addi	$r5, $r5, 1
 	lhi	$r1, [$r3 + 0]
-	bne	$r1, $r2, .LFAIL
+	beq	$r1, $r2, 1f
+	FAIL	1
+1:
 	addi	$r5, $r5, 1
 	lhi333	$r1, [$r3 + 0]
-	bne	$r1, $r2, .LFAIL
+	beq	$r1, $r2, 1f
+	FAIL	2
+1:
 	addi	$r5, $r5, 1
-	lh	$r1, [$r3 + $r0]
-	bne	$r1, $r2, .LFAIL
+	lh	$r1, [$r3 + $r4]
+	beq	$r1, $r2, 1f
+	FAIL	3
+1:
 
 	move	$r2, 0xffff8384
 	addi	$r5, $r5, 1
 	lhsi	$r1, [$r3 + 0]
-	bne	$r1, $r2, .LFAIL
+	beq	$r1, $r2, 1f
+	FAIL	4
+1:
 	addi	$r5, $r5, 1
-	lhs	$r1, [$r3 + $r0]
-	bne	$r1, $r2, .LFAIL
+	lhs	$r1, [$r3 + $r4]
+	beq	$r1, $r2, 1f
+	FAIL	5
+1:
 
 
 	! BYTE
@@ -58,26 +68,36 @@ main:
 	la	$r3, BYTE
 	addi	$r5, $r5, 1
 	lbi	$r1, [$r3 + 0]
-	bne	$r1, $r2, .LFAIL
+	beq	$r1, $r2, 1f
+	FAIL	6
+1:
 	addi	$r5, $r5, 1
 	lbi333	$r1, [$r3 + 0]
-	bne	$r1, $r2, .LFAIL
+	beq	$r1, $r2, 1f
+	FAIL	7
+1:
 	addi	$r5, $r5, 1
-	lb	$r1, [$r3 + $r0]
-	bne	$r1, $r2, .LFAIL
+	lb	$r1, [$r3 + $r4]
+	beq	$r1, $r2, 1f
+	FAIL	8
+1:
 
 	move	$r2, 0xffffff84
 	addi	$r5, $r5, 1
 	lbsi	$r1, [$r3 + 0]
-	bne	$r1, $r2, .LFAIL
+	beq	$r1, $r2, 1f
+	FAIL	9
+1:
 	addi	$r5, $r5, 1
-	lbs	$r1, [$r3 + $r0]
-	bne	$r1, $r2, .LFAIL
+	lbs	$r1, [$r3 + $r4]
+	beq	$r1, $r2, 1f
+	FAIL	0xa
+1:
 
 	! Set to little endian
 	mfsr	$r3, $psw
-	li	$r4, ~32
-	and	$r3, $r3, $r4	!psw.be
+	li	$r2, ~32
+	and	$r3, $r3, $r2	!psw.be
 	mtsr	$r3, $psw
 
 	! HALF
@@ -85,59 +105,67 @@ main:
 	la	$r3, HALF
 	addi	$r5, $r5, 1
 	lhi	$r1, [$r3 + 0]
-	bne	$r1, $r2, .LFAIL
+	beq	$r1, $r2, 1f
+	FAIL	0xb
+1:
 	addi	$r5, $r5, 1
 	lhi333	$r1, [$r3 + 0]
-	bne	$r1, $r2, .LFAIL
+	beq	$r1, $r2, 1f
+	FAIL	0xc
+1:
 	addi	$r5, $r5, 1
-	lh	$r1, [$r3 + $r0]
-	bne	$r1, $r2, .LFAIL
+	lh	$r1, [$r3 + $r4]
+	beq	$r1, $r2, 1f
+	FAIL	0xd
+1:
 
 	move	$r2, 0xffff8483
 	addi	$r5, $r5, 1
 	lhsi	$r1, [$r3 + 0]
-	bne	$r1, $r2, .LFAIL
+	beq	$r1, $r2, 1f
+	FAIL	0xe
+1:
 	addi	$r5, $r5, 1
-	lhs	$r1, [$r3 + $r0]
-	bne	$r1, $r2, .LFAIL
+	lhs	$r1, [$r3 + $r4]
+	beq	$r1, $r2, 1f
+	FAIL	0xf
+1:
 
 	! BYTE
 	move	$r2, 0x84
 	la	$r3, BYTE
 	addi	$r5, $r5, 1
 	lbi	$r1, [$r3 + 0]
-	bne	$r1, $r2, .LFAIL
+	beq	$r1, $r2, 1f
+	FAIL	0x10
+1:
 	addi	$r5, $r5, 1
 	lbi333	$r1, [$r3 + 0]
-	bne	$r1, $r2, .LFAIL
+	beq	$r1, $r2, 1f
+	FAIL	0x11
+1:
 	addi	$r5, $r5, 1
-	lb	$r1, [$r3 + $r0]
-	bne	$r1, $r2, .LFAIL
+	lb	$r1, [$r3 + $r4]
+	beq	$r1, $r2, 1f
+	FAIL	0x12
+1:
 
 	move	$r2, 0xffffff84
 	addi	$r5, $r5, 1
 	lbsi	$r1, [$r3 + 0]
-	bne	$r1, $r2, .LFAIL
+	beq	$r1, $r2, 1f
+	FAIL	0x13
+1:
 	addi	$r5, $r5, 1
-	lbs	$r1, [$r3 + $r0]
-	bne	$r1, $r2, .LFAIL
+	lbs	$r1, [$r3 + $r4]
+	beq	$r1, $r2, 1f
+	FAIL	0x14
+1:
+
+
 
 	addi	$r5, $r5, -20
-	bnez	$r5, .LFAIL
-
-.LPASS:
-	la	$r0, .Lpstr
-	bal	puts
-	movi	$r0, 0
-	syscall 1
-
-.LFAIL:
-	la	$r0, .Lfstr
-	bal	puts
-	movi	$r0, 1
-	syscall 1
-
-.Lpstr:
-	.string "pass\n"
-.Lfstr:
-	.string "fail\n"
+	beqz	$r5, 1f
+	FAIL	0x15
+1:
+	PASS
