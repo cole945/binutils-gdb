2d6af73320092861b7c06132d9306e973a935d4b
Cole Wang <cole@andestech.com>
2012-05-14 14:10:55 +0800

    Fix NULL check which should not be removed.

--- gdb/nds32-tdep.c
+++ gdb/nds32-tdep.c
@@ -282,6 +282,9 @@ nds32_types (struct gdbarch *gdbarch, const char *reg_name)
   struct type *type, *stype1, *stype2;
   struct field *f;
 
+  if (reg_name == NULL || reg_name[0] == '\0')
+    return NULL;
+
   if (tdep->nds32_types.next == NULL)
     nds32_list_init (&tdep->nds32_types);
   else
